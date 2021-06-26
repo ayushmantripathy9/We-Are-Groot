099b7227d97f9bb33bcc9d6d9c607a2d44549257
Ayushman Tripathy <ayushmantripathy9@gmail.com>
2021-06-27 03:54:46 +0530

    [Backend] Room creation and joining - Added Room model and its serializer - Made the room view - Creating and Joining a meeting functionalities added

---
+++ groot-backend/django/code/groot/models/room.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.conf import settings
+
+class Room(models.Model):
+    """
+        This is the Room Model.
+        This would contain the rooms created by users for interaction
+    """
+
+    # stores the name of the rooom
+    room_name = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+    
+    # stores the unique room_code
+    room_code = models.CharField(
+        max_length=9,
+        null=False,
+        blank=False
+    )
+
+    # the participants who are a part of the room
+    participants = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='rooms_of_user',
+        blank=True
+    )
+
+    # creation time of the room
+    start_time = models.DateTimeField(
+        auto_now_add=True
+    ) 
+
+    end_time = models.DateTimeField(
+        null=True,
+        blank=True
+    )
+
+    def __str__(self) :
+        return f"Room :: name : {self.room_name}, code : {self.room_code}"
