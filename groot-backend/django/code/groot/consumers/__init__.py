2f718f5aa666c7a493ad20fd1bf952f7ec1fb267
Ayushman Tripathy <ayushmantripathy9@gmail.com>
2021-07-09 09:17:31 +0530

    [Backend] Chat Backend Implemented - Added Chat Consumer to handle Chat WS requests - Made the required ws api endpoint for chat messages

--- groot-backend/django/code/groot/consumers/__init__.py
+++ groot-backend/django/code/groot/consumers/__init__.py
@@ -1,2 +1,3 @@
 from groot.consumers.room import RoomConsumer
 from groot.consumers.signalling import SignallingConsumer
+from groot.consumers.chat import ChatConsumer
---
+++ groot-backend/django/code/groot/consumers/chat.py
@@ -0,0 +1,101 @@
+import json
+
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+from groot.consumers.messageTypes import chat_events
+
+from groot.models import Message, Room
+
+from groot.serializers import UserGetSerializer, MessageGetSerializer
+
+class ChatConsumer(WebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+
+    def connect(self):    
+        self.room_code = self.scope['url_route']['kwargs']['room_code']
+        self.user = self.scope['user']
+
+        self.chat_group_name = f'chat-{self.room_code}'
+        try:
+            room = Room.objects.get(room_code=self.room_code)
+
+            async_to_sync(self.channel_layer.group_add)(
+                self.chat_group_name, 
+                self.channel_name
+            )
+
+            self.accept()
+
+            room_messages = Message.objects.filter(room_id=room.id).order_by('time_sent')
+
+            message_data = {
+                'type': chat_events.MESSAGES_SENT_BEFORE,
+                'data': MessageGetSerializer(room_messages, many=True).data
+            }
+
+            self.send(text_data=json.dumps(message_data))
+
+        
+        except Room.DoesNotExist:
+            print("No such room exists")
+            self.close()
+
+    def disconnect(self, code):
+        self.user = self.scope['user']
+
+        async_to_sync(self.channel_layer.group_discard)(
+            self.chat_group_name,
+            self.channel_name
+        )
+
+    def receive(self, text_data):
+        received_data = json.loads(text_data)
+        type = received_data.get('type')
+        content = received_data.get('content')
+        
+        if(content):
+            try:
+                room = Room.objects.get(room_code=self.room_code)
+
+                message = Message(
+                    sender=self.user,
+                    content=content,
+                    room=room
+                )
+                message.save()
+
+                serializer = MessageGetSerializer(message)
+
+                async_to_sync(self.channel_layer.group_send)(
+                    self.chat_group_name,
+                    {
+                        'type': "send_message_to_all",
+                        'message': {
+                            'type': chat_events.NEW_MESSAGE,
+                            'data': serializer.data
+                        }
+                    }
+                )
+
+            except:
+                self.close()
+
+        elif(type == chat_events.GET_MESSAGES_SENT_BEFORE):
+            room_messages = Message.objects.filter(room_code=self.room_code).order_by('time_sent')
+
+            message_data = {
+                'type': chat_events.MESSAGES_SENT_BEFORE,
+                'data': MessageGetSerializer(room_messages, many=True).data
+            }
+
+            self.send(text_data=json.dumps(message_data))
+
+        else:
+            pass        
+
+    
+    def send_message_to_all(self, event):
+        message = event['message']
+        self.send(text_data=json.dumps(message))
+
