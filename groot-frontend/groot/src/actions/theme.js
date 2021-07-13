e435e08d8eca5b5f0446e901dd9de9332b929638
Ayushman Tripathy <ayushmantripathy9@gmail.com>
2021-07-13 13:01:55 +0530

    [Frontend] Documentation for Actions Complete

--- groot-frontend/groot/src/actions/theme.js
+++ groot-frontend/groot/src/actions/theme.js
@@ -1,5 +1,10 @@
 import { CHANGE_THEME } from "./types"
 
+/**
+ * Changes the current theme of the app
+ * @param {string} theme_state the current theme state ('dark' or 'light')
+ * @returns a dispatch method to update the current theme state in the redux store
+ */
 export const changeTheme = (theme_state) => {
     return dispatch => {
         dispatch({
--- groot-frontend/groot/src/actions/user.js
+++ groot-frontend/groot/src/actions/user.js
@@ -7,7 +7,13 @@ import {
     USER_LOGOUT
 } from './types'
 
-
+/**
+ * Verifies whether the user is logged in or not
+ * @returns a dispatch method to initialize the userInfo in the redux store
+ * 
+ * Makes a request to the verify api endpoint in the backend
+ * Based on the response, it sets the initial state of the redux store's userInfo
+ */
 export const verifyLoggedIn = () => {
     return dispatch => {
         axios.get(
@@ -63,6 +69,15 @@ export const verifyLoggedIn = () => {
     }
 }
 
+/**
+ * Logs In the user into the app
+ * @param {string} provider the auth provider ('github' or 'google')
+ * @param {string} auth_code auth code obtained after user-confirmation in the provider-auth page
+ * @returns and unsuccessful user_login response in case of failure
+ * 
+ * Makes request to the backend's required endpoint and logs in the user into the app
+ * This is verified by the verify endpoint once the user tries to access homepage (to which it is redirected to after this process)
+ */
 export const userLogin = (provider, auth_code) => {
     return dispatch => {
         
@@ -96,6 +111,9 @@ export const userLogin = (provider, auth_code) => {
     }
 }
 
+/**
+ * Logs out the user of our app
+ */
 export const userLogout = () => {
     return dispatch => {
         axios({
